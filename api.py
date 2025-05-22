5302b79c0ea0d22e38558f202e5ec27974704a75
James Shiffer <jshiffer@ucla.edu>
2025-05-22 10:25:45 -0700

    Decrypt images when X-Obfuscation header present

--- api.py
+++ api.py
@@ -2,7 +2,12 @@
 # Copyright (c) 2020  James Shiffer
 # This file contains all the API calls made to archive.org.
 
-import json, logging, lxml.etree, re, requests, sched, time
+import base64, json, logging, lxml.etree, re, requests, sched, time
+
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA1
+from Crypto.Util import Counter
+
 
 class ArchiveReaderClient:
 
@@ -17,9 +22,11 @@ class ArchiveReaderClient:
         self.timer = sched.scheduler(time.time, time.sleep)
 
 
-    # Borrows a book. You should use the scheduler instead of calling this
-    # method directly.
     def borrow_book(self, book_id):
+        """
+        Borrows a book. You should use the scheduler instead of calling this
+        method directly.
+        """
         logging.debug('attempting to borrow book')
 
         # borrowing is done in two phases: 'browse_book' and 'grant_access'
@@ -50,9 +57,11 @@ class ArchiveReaderClient:
             self.token = js['value']
 
 
-    # Renews a loaned book, which must be borrowed before calling this method.
-    # You should use the scheduler instead of calling this directly.
     def renew_book(self):
+        """
+        Renews a loaned book, which must be borrowed before calling this method.
+        You should use the scheduler instead of calling this directly.
+        """
         if not self.book_id:
             logging.error('no book_id; you need to borrow a book first.')
             raise AssertionError
@@ -73,24 +82,30 @@ class ArchiveReaderClient:
             self.token = js['token']
 
 
-    # Performs one renewal and schedules the next one for two minutes in the future.
     def schedule_renew_book(self):
+        """
+        Performs one renewal and schedules the next one for two minutes in the future.
+        """
         logging.debug('time is %d, time to renew book again' % time.time())
         self.renew_book()
         self.timer.enter(120, 1, self.schedule_renew_book)
 
 
-    # Borrows a book and then automatically renews it every two minutes.
     def schedule_loan_book(self, book_id):
+        """
+        Borrows a book and then automatically renews it every two minutes.
+        """
         # first, borrow & renew the book once
         logging.debug('scheduler running borrow/renew for the first time')
         self.borrow_book(book_id)
         self.schedule_renew_book()
 
 
-    # Finds the book metadata, including book title and page URLs, and
-    # returns the page count.
     def fetch_book_metadata(self):
+        """
+        Finds the book metadata, including book title and page URLs, and
+        returns the page count.
+        """
         if not self.book_id:
             logging.error('no book_id; you need to borrow a book first.')
             raise AssertionError
@@ -105,7 +120,7 @@ class ArchiveReaderClient:
             logging.error('bookreader metadata is missing URL field')
             raise AssertionError
 
-        # call the endpoint and viola, we have all the info we could ever
+        # call the endpoint and voila, we have all the info we could ever
         # want about our book.
         res = self.session.get('https:' + reader['url'])
         js = res.json()
@@ -125,8 +140,10 @@ class ArchiveReaderClient:
         return len(self.book_page_urls)
 
 
-    # Downloads a single page of a book. Call fetch_book_metadata() first.
     def download_page(self, i, scale=0):
+        """
+        Downloads a single page of a book. Call fetch_book_metadata() first.
+        """
         if not self.book_meta:
             logging.error('no book_meta; you must fetch the metadata first.')
             raise AssertionError
@@ -135,14 +152,56 @@ class ArchiveReaderClient:
             logging.error('page index out of range')
             raise IndexError
 
-        res = self.session.get(self.book_page_urls[i] + "&scale=%d" % scale, headers={
+        img_url = self.book_page_urls[i] + "&scale=%d&rotate=0" % scale
+        res = self.session.get(img_url, headers={
             'referer': self.URL_FORMAT % ('details/' + self.book_id)
         })
-        return res.content
+
+        image_data = res.content
+        obfuscation_key = res.headers.get('x-obfuscate')
+        if obfuscation_key:
+            version, counter = obfuscation_key.split('|')
+            if version != '1':
+                raise AssertionError('obfuscation version %s not supported' % version)
+            
+            counter = base64.b64decode(counter)
+            aes_key = re.sub(r'https?:\/\/.*?\/', '/', img_url)
+
+            # decrypt first 1024 bytes of image data
+            decrypted_part = self.decrypt(image_data[:1024], aes_key, counter)
+            # replace the first 1024 bytes of image data with the decrypted data
+            decrypted_buffer = bytearray(image_data)
+            decrypted_buffer[:1024] = decrypted_part
+            image_data = bytes(decrypted_buffer)
+
+        return image_data
 
 
-    # Logs a user in to their archive.org account.
+    def decrypt(self, data: bytes, aes_key: str, counter: bytes):
+        """
+        Decrypts the data with AES-CTR using the given key.
+        """
+        # hash the AES key using SHA-1
+        sha1 = SHA1.new()
+        sha1.update(aes_key.encode('utf-8'))
+        aes_key_hashed = sha1.digest()[:16]  # use first 16 bytes
+
+        # create a counter of length 64-bit
+        ctr = Counter.new(64, prefix=counter[:8], initial_value=int.from_bytes(counter[8:], 'big'))
+
+        # create AES cipher in CTR mode
+        cipher = AES.new(aes_key_hashed, AES.MODE_CTR, counter=ctr)
+
+        # decrypt the buffer
+        plaintext = cipher.decrypt(data)
+
+        return plaintext
+
+
     def login(self, email, password):
+        """
+        Logs a user in to their archive.org account.
+        """
         # get cookies
         self.session.get(self.URL_FORMAT % 'account/login')
 
